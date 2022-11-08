5961fdfad3cb8b7a60ef9dc2f00b0c5cc91ff9c3
Jayneet123 <68434045+Jayneet123@users.noreply.github.com>
2022-11-08 21:27:32 +0530

    ...

--- simulationScript.js
+++ simulationScript.js
@@ -1,30 +1,25 @@
-const svg = document.getElementById('svg');
-let startX, startY, elementX, elementY, element;
+// const svg = document.getElementById('svg');
+// let startX, startY, elementX, elementY, element;
 
-svg.addEventListener('mousedown', e => {
- const className = e.target.getAttributeNS (null, 'class');
- if (className.indexOf('draggable') >= 0) {
-   startX = e.offsetX;
-   startY = e.offsetY;
-   element = e.target;
-   elementX = +element.getAttributeNS(null,'x');
-   elementY = +element.getAttributeNS(null,'x');
-   svg.addEventListener('mousemove', onMouseMove);
- }
-});
+// svg.addEventListener('mousedown', e => {
+//  const className = e.target.getAttributeNS (null, 'class');
+//  if (className.indexOf('draggable') >= 0) {
+//    startX = e.offsetX;
+//    startY = e.offsetY;
+//    element = e.target;
+//    elementX = +element.getAttributeNS(null,'x');
+//    elementY = +element.getAttributeNS(null,'x');
+//    svg.addEventListener('mousemove', onMouseMove);
+//  }
+// });
 
-onMouseMove = e => {
- let x = e.offsetX;
- let y = e.offsetY;
- element.setAttributeNS(null,'x', elementX+x-startX);
- element.setAttributeNS(null,'y', elementY+y-startY);
-};
+// onMouseMove = e => {
+//  let x = e.offsetX;
+//  let y = e.offsetY;
+//  element.setAttributeNS(null,'x', elementX+x-startX);
+//  element.setAttributeNS(null,'y', elementY+y-startY);
+// };
 
-svg.addEventListener('mouseup', e => {
- svg.removeEventListener('mousemove', onMouseMove);
-});
-
-// import TweenMax from "gsap/TweenMax";
-// import Draggable from "gsap/Draggable"; 
-
-// Draggable.create(".draggable");
+// svg.addEventListener('mouseup', e => {
+//  svg.removeEventListener('mousemove', onMouseMove);
+// });
