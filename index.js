e203743946f4331bbd0267422522ed7523066ab4
Jayneet123 <68434045+Jayneet123@users.noreply.github.com>
2022-11-09 19:39:25 +0530

    improved drag and drop

--- index.js
+++ index.js
@@ -25,30 +25,45 @@ document.addEventListener('DOMContentLoaded', (event) => {
       this.classList.remove('over');
     }
   
-    let items = document.querySelectorAll('.container1 .box');
-    items.forEach(function(item) {
-      item.addEventListener('dragstart', handleDragStart);
-      item.addEventListener('dragover', handleDragOver);
-      item.addEventListener('dragenter', handleDragEnter);
-      item.addEventListener('dragleave', handleDragLeave);
-      item.addEventListener('dragend', handleDragEnd);
-      item.addEventListener('drop', handleDrop);
-    });
+    // let items = document.querySelectorAll('.container1 .box');
+    // items.forEach(function(item) {
+    //   item.addEventListener('dragstart', handleDragStart);
+    //   item.addEventListener('dragover', handleDragOver);
+    //   item.addEventListener('dragenter', handleDragEnter);
+    //   item.addEventListener('dragleave', handleDragLeave);
+    //   item.addEventListener('dragend', handleDragEnd);
+    //   item.addEventListener('drop', handleDrop);
+    // });
+    const source = document.getElementById('rect');
+    const target = document.getElementById('square1');
+    source.addEventListener("dragstart", handleDragStart)
+    source.addEventListener("dragend",handleDragEnd)
+    target.addEventListener("dragover", handleDragOver)
+    target.addEventListener("drop", handleDrop)
+  
   function handleDragStart(e) {
     this.style.opacity = '0.4';
   
     dragSrcEl = this;
   
     e.dataTransfer.effectAllowed = 'move';
-    e.dataTransfer.setData('text/html', this.innerHTML);
+    // e.dataTransfer.setData('squareOnRect', this.innerHTML);
+    e.dataTransfer.setData('squareOnRect',e.target.id);
   }
   function handleDrop(e) {
     e.stopPropagation();
+    dragSrcEl = this;
   
-    if (dragSrcEl !== this) {
-      dragSrcEl.innerHTML = this.innerHTML;
-      this.innerHTML = e.dataTransfer.getData('text/html');
+    // if (dragSrcEl !== this) {
+    //   dragSrcEl.innerHTML = this.innerHTML;
+    //   this.innerHTML = e.dataTransfer.getData('squareOnRect');
+    // }
+    if(dragSrcEl.id !== document.getElementById('square1')){
+      const data = e.dataTransfer.getData('squareOnRect');
+      e.target.appendChild(document.getElementById(data));
+      console.log("Success");
     }
+    
   
     return false;
   }
